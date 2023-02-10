4c6cfad285a02b64ce3042cc8c95f777f055b1b4
GEZUS95 <ceesgribnau@hotmail.com>
2023-02-10 17:30:20 +0100

    created userRepository and userservice. removed switch router

--- app/repositories/userrepository.php
+++ app/repositories/userrepository.php
@@ -16,4 +16,65 @@ class UserRepository extends Repository{
             echo $e;
         }
     }
+
+    public function insertOne(User $user)
+    {
+        try {
+            $stmt = $this->connection->prepare("INSERT INTO user ('name', 'email', 'passwordhash', 'description', 'role_id')");
+            $name = $user->getName(); //todo: blijkbaar werkt het niet als deze statement direct in de bindparam zit
+            $stmt->bindParam('ssssi', $name, $user->getEmail(), $user->getPasswordhash(), $user->getDescription(), $user->getRole());
+
+            $stmt->execute();
+            return $this->getOneByEmail($user->getEmail());
+        }
+        catch (PDOException $e){
+            echo $e;
+        }
+    }
+
+    public function getOneByEmail(string $email)
+    {
+        try {
+            $stmt = $this->connection->prepare("SELECT * FROM user WHERE email = ? LIMIT 1");
+            $stmt->bindParam('s', $email);
+            $stmt->execute();
+
+            $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
+            return $stmt->fetchAll();
+
+        } catch (PDOException $e)
+        {
+            echo $e;
+        }
+    }
+    public function getOneByName(string $name)
+    {
+        try {
+            $stmt = $this->connection->prepare("SELECT * FROM user WHERE name = ? LIMIT 1");
+            $stmt->bindParam('s', $name);
+            $stmt->execute();
+
+            $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
+            return $stmt->fetchAll();
+
+        } catch (PDOException $e)
+        {
+            echo $e;
+        }
+    }
+    public function getOneById(int $id)
+    {
+        try {
+            $stmt = $this->connection->prepare("SELECT * FROM user WHERE id = ? LIMIT 1");
+            $stmt->bindParam('i', $id);
+            $stmt->execute();
+
+            $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
+            return $stmt->fetchAll();
+
+        } catch (PDOException $e)
+        {
+            echo $e;
+        }
+    }
 }
--- app/services/userservice.php
+++ app/services/userservice.php
@@ -3,8 +3,32 @@ require __DIR__ . '/../repositories/userrepository.php';
 
 class UserService
 {
-    public function getAll() {
-        $repository = new UserRepository();
-        return $repository->getAll();
+
+    private $REPOSITORY;
+    public function __construct()
+    {
+        $this->REPOSITORY = new UserRepository();
     }
+
+    public function getAll() {
+        return $this->REPOSITORY->getAll();
+    }
+    public function updateOne(User $user){
+        $this->REPOSITORY->updateOne($user);
+    }
+    public function insertOne(User $user){
+        $this->REPOSITORY->insertOne($user);
+    }
+
+    public function getOneByEmail(string $email){
+        $this->REPOSITORY->getOneByEmail($email);
+    }
+    public function getOneByName(string $name){
+        $this->REPOSITORY->getOneByEmail($name);
+    }
+    public function getOneById(int $id){
+        $this->REPOSITORY->getOneByEmail($id);
+    }
+
+
 }
