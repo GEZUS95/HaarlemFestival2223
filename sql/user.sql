012e7548d06aab09ca0cd45a943cef731bfbdb32
GEZUS95 <ceesgribnau@hotmail.com>
2023-02-13 22:12:45 +0100

    hashed passwords for standard server data

--- sql/user.sql
+++ sql/user.sql
@@ -13,6 +13,8 @@ CREATE TABLE user(
 );
 
 # Voer data in de tabel
-INSERT INTO user VALUES (1, 'gebruiker', 'gebruiker@email.com', 'welkom1234', 'dit is een normale test gebruiker', 1);
-INSERT INTO user VALUES (2, 'admin', 'admin@email.com', 'welkom1234', 'dit is een admin account', 2);
-INSERT INTO user VALUES (3, 'super-admin', 'superadmin@email.com', 'welkom1234','dit is een Super-administrator account',3);
+INSERT INTO user VALUES (1, 'gebruiker', 'gebruiker@email.com', '$2y$10$/LNXF0yNrbWuzNXXGU8uP.wpWnkTbqzHYx0Ex.Ks2t8p4tBDarnJO', 'dit is een normale test gebruiker', 1);
+INSERT INTO user VALUES (2, 'admin', 'admin@email.com', '$2y$10$/LNXF0yNrbWuzNXXGU8uP.wpWnkTbqzHYx0Ex.Ks2t8p4tBDarnJO', 'dit is een admin account', 2);
+INSERT INTO user VALUES (3, 'super-admin', 'superadmin@email.com', '$2y$10$/LNXF0yNrbWuzNXXGU8uP.wpWnkTbqzHYx0Ex.Ks2t8p4tBDarnJO','dit is een Super-administrator account',3);
+
+# passwords zijn: welkom1234
