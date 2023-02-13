4bf5f6d2bf79b5d1264c0c4e3236a4c93bbb3c13
GEZUS95 <ceesgribnau@hotmail.com>
2023-02-13 18:22:25 +0100

    introduced data to users view and added admin controller

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -1,1 +1,5 @@
 <?php
+include __DIR__ . '/admin-header.php';
+?>
+
+<h1>Welcome to the admin panel</h1>
--- app/views/admin/users.php
+++ app/views/admin/users.php
@@ -1,1 +1,36 @@
 <?php
+include __DIR__ . '/admin-header.php';
+?>
+
+<h1>Welcome to the users panel</h1>
+
+<table>
+    <thead>
+    <tr>
+        <th>Id</th>
+        <th>username</th>
+        <th>email</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($model as $user) {
+        ?>
+
+        <tr>
+            <td><?php $user->getId() ?></td>
+            <td><?php $user->getName() ?></td>
+            <td><?php $user->getEmail() ?></td>
+            <td>
+                <button>Change Password</button>
+                <button>Update User</button>
+                <button>Delete User</button>
+            </td>
+        </tr>
+
+        <?php
+    }
+    ?>
+    </tbody>
+</table>
