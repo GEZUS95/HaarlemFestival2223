8f63f1614c7786ffdaf3234963c480944c6e0c40
FlorisBeentjes <666792@student.inholland.nl>
2023-02-21 19:21:42 +0100

    Email works now

--- app/services/EmailService.php
+++ app/services/EmailService.php
@@ -4,13 +4,17 @@ namespace services;
 use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Transport\Smtp\Auth\LoginAuthenticator;
+use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
+use Symfony\Component\Mailer\Transport\Smtp\SmtpTransport;
 use Symfony\Component\Mime\Email;
 
 Class EmailService
 {
     public MailerInterface $mailer;
     public function __construct(){
-        $transport = Transport::fromDsn('smtp://no-reply@haarlemfestival.com:no-reply2022@mail.haarlemfestival.com:587');
+        //Code for making the mailer (secure)
+        $transport = Transport::fromDsn('smtp://no-reply@haarlemfestival.com:no-reply2022@mail.axc.nl:465');
         $this->mailer = new Mailer($transport);
     }
     public function sendEmail(string $addressFrom, string $addressTo, string $subject, String $message): void
