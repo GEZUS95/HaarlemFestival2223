0104d55bc1da72ad87b4a0fcb7416cf36a41cb2a
GEZUS95 <ceesgribnau@hotmail.com>
2023-03-07 12:04:35 +0100

    added XSS for full website

--- app/public/index.php
+++ app/public/index.php
@@ -5,4 +5,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__ . '/../routers/Router.php';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
 $router->run();
