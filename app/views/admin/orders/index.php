17a8e756ecbf2aad354eb751ed87e535f193ae96
GEZUS95 <ceesgribnau@hotmail.com>
2023-03-22 21:08:07 +0100

    removed unused button

--- app/views/admin/orders/index.php
+++ app/views/admin/orders/index.php
@@ -10,7 +10,7 @@ include_once __DIR__ . '/../admin-header.php';
         <th scope="col">Userid</th>
         <th scope="col">Share Id</th>
         <th scope="col">Status</th>
-        <th scope="col"><a href="/admin/orders/create" class="btn btn-success disabled" role="button">Create Order</a></th>
+        <th scope="col"></th>
     </tr>
     </thead>
     <tbody>
