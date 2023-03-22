7aa7493dec15d380689e5809a4c9b280329ff397
GEZUS95 <ceesgribnau@hotmail.com>
2023-03-22 21:06:29 +0100

    added extra funcion for downloading invoice

--- app/helpers/PDFHelper.php
+++ app/helpers/PDFHelper.php
@@ -74,6 +74,33 @@ class PDFHelper
         $pdf->writeHTML($html, true, false, true, false, '');
 
         // Output the generated PDF file
+        return $pdf->Output($orderNumber . '_invoice.pdf', 'S');
+    }
+
+    public function generateInvoiceDownload($customerName, $orderNumber, $orderDate, $items)
+    {
+        // Instantiate the TCPDF class
+        $pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8', false);
+
+        // Set the document properties
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetAuthor('Haarlem Festival');
+        $pdf->SetTitle('Invoice of order ' . $orderNumber);
+        $pdf->SetSubject('Invoice');
+
+        // Set the page margins
+        $pdf->SetMargins(10, 10, 10);
+
+        // Read the PHP template file and render its contents as HTML, passing in the variables
+        ob_start(); // Start output buffering
+        include __DIR__ . '/../views/pdftemplates/invoice.php';
+        $html = ob_get_clean(); // Get the contents of the output buffer and clear it
+
+        // Write the HTML to the PDF
+        $pdf->AddPage();
+        $pdf->writeHTML($html, true, false, true, false, '');
+
+        // Output the generated PDF file
         return $pdf->Output($orderNumber . '_invoice.pdf', 'I');
     }
 }
